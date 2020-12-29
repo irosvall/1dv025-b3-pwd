6a207af352be72c258714eec07de40a510eb89e0
Ida Rosvall <ir222gn@student.lnu.se>
2020-12-29 14:16:08 +0100

    Init project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 /**
  * The main script file of the application.
  *
- * @author // TODO: YOUR NAME <YOUR EMAIL>
+ * @author Ida Rosvall <ir222gn@student.lnu.se>
  * @version 1.0.0
  */
 
-console.log('TODO: Start working on the assignment')
