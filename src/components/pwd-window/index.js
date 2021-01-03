01e1544be95d22d7534b1f11721e1f06550f9145
Ida Rosvall <ir222gn@student.lnu.se>
2021-01-03 15:01:32 +0100

    Make pwd-window custom element draggable

--- src/components/pwd-window/index.js
+++ src/components/pwd-window/index.js
@@ -81,6 +81,20 @@ customElements.define('pwd-window',
       /* ------------HTML ELEMENTS----------- */
 
       /**
+       * A div element representing the entire window.
+       *
+       * @type {HTMLElement}
+       */
+      this._window = this.shadowRoot.querySelector('#window')
+
+      /**
+       * A div element containing the header of the window.
+       *
+       * @type {HTMLElement}
+       */
+      this._windowHeader = this.shadowRoot.querySelector('#windowHeader')
+
+      /**
        * A div element containing the name of the application to be displayed.
        *
        * @type {HTMLElement}
@@ -89,8 +103,76 @@ customElements.define('pwd-window',
 
       /* ------------OTHER PROPERTIES----------- */
 
+      /**
+       * The mouse x-coordinate.
+       *
+       * @type {number}
+       */
+      this._positionX = 0
+
+      /**
+       * The mouse y-coordinate.
+       *
+       * @type {number}
+       */
+      this._positionY = 0
+
+      /**
+       * A Boolean indicating whether or not the window is being dragged.
+       *
+       * @type {Boolean}
+       */
+      this._isDraging = false
+
       /* ------------EVENT HANDLERS----------- */
 
+      /**
+       * Handles mousedown events for when the user press down
+       * the mouse button on the window's header.
+       *
+       * @param {Event} event - The mousedown event.
+       */
+      this._onMousedown = event => {
+        // Prevent the text of being highlighted.
+        event.preventDefault()
+
+        this._positionX = event.clientX
+        this._positionY = event.clientY
+        this._isDraging = true
+      }
+
+      /**
+       * Inspiration how to move element gathered from: 
+       * https://www.w3schools.com/howto/howto_js_draggable.asp [visited: 2021-01-03]
+       * 
+       * Handles mousemove events for when the user's cursor
+       * is moved inside the window's header.
+       *
+       * @param {Event} event - The mousemove event.
+       */
+      this._onMousemove = event => {
+        if (this._isDraging === true) {
+          const newPositionX = this._positionX - event.clientX
+          const newPositionY = this._positionY - event.clientY
+          this._positionX = event.clientX
+          this._positionY = event.clientY
+          
+          this._window.style.left = `${this._window.offsetLeft - newPositionX}px`
+          this._window.style.top = `${this._window.offsetTop - newPositionY}px`
+        }
+      }
+
+      /**
+       * Handles mouseup events for when the user releases 
+       * the mouse button from the window's header.
+       */
+      this._onMouseup = () => {
+        if (this._isDraging === true) {
+          this._positionX = 0
+          this._positionY = 0
+          this._isDraging = false
+        }
+      }
     }
 
     /**
@@ -117,14 +199,18 @@ customElements.define('pwd-window',
      * Called after the element is inserted into the DOM.
      */
     connectedCallback () {
-
+      this._windowHeader.addEventListener('mousedown', this._onMousedown)
+      this._windowHeader.addEventListener('mousemove', this._onMousemove)
+      this._windowHeader.addEventListener('mouseup', this._onMouseup)
     }
 
     /**
      * Called after the element has been removed from the DOM.
      */
     disconnectedCallback () {
-
+      this._windowHeader.removeEventListener('mousedown', this._onMousedown)
+      this._windowHeader.removeEventListener('mousemove', this._onMousemove)
+      this._windowHeader.removeEventListener('mouseup', this._onMouseup)
     }
   }
 )
