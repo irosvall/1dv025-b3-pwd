1815c63c4bb2207eac4a0176b7309ec12fbee2ab
Ida Rosvall <ir222gn@student.lnu.se>
2021-01-05 14:37:28 +0100

    Add icon functionality

--- src/components/pwd-icon/index.js
+++ src/components/pwd-icon/index.js
@@ -5,24 +5,23 @@
  * @version 1.0.0
  */
 
+// Dependencies
+import '../pwd-window/'
+
 /**
  * Define template.
  */
 const template = document.createElement('template')
 template.innerHTML = `
   <style>
-    #icon {
+    :host {
       display: inline-block;
     }
 
-    slot {
-      width: 100%;
-      height: 100%;
-    }
-
     ::slotted(*) {
-      width: 50px;
-      height: 50px;
+      display: block;
+      width: 60px;
+      height: 60px;
     }
   </style>
 
@@ -54,23 +53,100 @@ customElements.define('pwd-icon',
 
       /* ------------HTML ELEMENTS----------- */
 
+      /**
+       * A div element representing the icon.
+       *
+       * @type {HTMLElement}
+       */
+      this._icon = this.shadowRoot.querySelector('#icon')
+
       /* ------------OTHER PROPERTIES----------- */
 
+      /**
+       * The title of the application.
+       *
+       * @type {string}
+       */
+      this._title = ''
+
+      /**
+       * The application name to create a html element with.
+       *
+       * @type {string}
+       */
+      this._app = ''
+
       /* ------------EVENT HANDLERS----------- */
+
+      /**
+       * Handles click events.
+       */
+      this._onClick = () => {
+        this._startApplication()
+      }
+      
+      /**
+       * Handles keydown events.
+       *
+       * @param {KeyboardEvent} event - The keyboard event.
+       */
+      this._onKeydown = event => {
+        if (event.code === 'Enter') {
+          this._startApplication()
+        }
+      }
+    }
+
+    /**
+     * Watches the attributes "inactive" and "hidden" for changes on the element.
+     *
+     * @returns {string[]} An array of the observed attribute's names.
+     */
+    static get observedAttributes () {
+      return ['app', 'title']
+    }
+
+    /**
+     * Called by the browser engine when an attribute changes.
+     *
+     * @param {string} name - Name of the attribute.
+     * @param {any} oldValue - The old attribute value.
+     * @param {any} newValue - The new attribute value.
+     */
+    attributeChangedCallback (name, oldValue, newValue) {
+      if (name === 'app') {
+        this._app = newValue
+      } else {
+        this._title = newValue
+      }
     }
 
     /**
      * Called after the element is inserted into the DOM.
      */
     connectedCallback () {
-
+      this._icon.addEventListener('click', this._onClick)
+      this._icon.addEventListener('keydown', this._onKeydown)
     }
 
     /**
      * Called after the element has been removed from the DOM.
      */
     disconnectedCallback () {
+      this._icon.removeEventListener('click', this._onClick)
+      this._icon.removeEventListener('keydown', this._onKeydown)
+    }
 
+    /**
+     * Renders the application in a pwd-window custom element to the body element.
+     */
+    _startApplication () {
+      const pwdWindow = document.createElement('pwd-window')
+      pwdWindow.setAttribute('name', this._title)
+      const app = document.createElement(this._app)
+      pwdWindow.appendChild(app)
+
+      document.body.appendChild(pwdWindow)
     }
   }
 )
