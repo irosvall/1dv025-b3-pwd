1cf06d77625087c312c8985299767a793e8d9af2
Ida Rosvall <ir222gn@student.lnu.se>
2021-01-09 19:01:03 +0100

    Add drawing functionality that gets buggy when drawing fast

--- src/components/drawing-app/index.js
+++ src/components/drawing-app/index.js
@@ -14,7 +14,6 @@ template.innerHTML = `
     :host {
       display: grid;
       grid-template-columns: 50px auto;
-      position: relative;
       width: 600px;
       height: 380px;
       border: solid 1px rgb(87, 87, 87);
@@ -26,13 +25,11 @@ template.innerHTML = `
 
     canvas {
       background-color: white;
-      width: 100%;
-      height: 100%;
     }
   </style>
 
   <div id="toolbar"></div>
-  <canvas></canvas>
+  <canvas width="550" height="380"></canvas>
 `
 
 /**
@@ -58,23 +55,114 @@ customElements.define('drawing-app',
 
       /* ------------HTML ELEMENTS----------- */
 
+      /**
+       * A canvas element for the user to draw on.
+       *
+       * @type {HTMLElement}
+       */
+      this._canvas = this.shadowRoot.querySelector('canvas')
+
       /* ------------OTHER PROPERTIES----------- */
 
+      /**
+       * The rendering context of the canvas.
+       *
+       * @type {HTMLElement}
+       */
+      this._context = this._canvas.getContext('2d')
+
+      /**
+       * A Boolean indicating whether or not the user is drawing.
+       *
+       * @type {boolean}
+       */
+      this._isDrawing = false
+
+      /**
+       * The color of the drawing stroke.
+       *
+       * @type {string}
+       */
+      this._color = 'black'
+
+      /**
+       * The line width of the drawing stroke.
+       *
+       * @type {number}
+       */
+      this._lineWidth = 3
+
       /* ------------EVENT HANDLERS----------- */
+
+      /**
+       * Handles mousedown events for when the user press down the mouse button on the canvas.
+       *
+       * Starts drawing.
+       *
+       * @param {Event} event - The mousedown event.
+       */
+      this._onMousedown = event => {
+        this._isDrawing = true
+        this._draw(event.offsetX, event.offsetY)
+      }
+
+      /**
+       * Handles mousemove events for when the user's cursor is moved inside the canvas.
+       *
+       * Draws.
+       *
+       * @param {Event} event - The mousemove event.
+       */
+      this._onMousemove = event => {
+        if (this._isDrawing === true) {
+          this._draw(event.offsetX, event.offsetY)
+        }
+      }
+
+      /**
+       * Handles mouseup events for when the user releases the mouse button from the canvas.
+       *
+       * Stops drawing.
+       */
+      this._onMouseup = () => {
+        if (this._isDrawing === true) {
+          this._isDrawing = false
+        }
+      }
     }
 
     /**
      * Called after the element is inserted into the DOM.
      */
     connectedCallback () {
-
+      this._canvas.addEventListener('mousedown', this._onMousedown)
+      this._canvas.addEventListener('mousemove', this._onMousemove)
+      this._canvas.addEventListener('mouseup', this._onMouseup)
     }
 
     /**
      * Called after the element has been removed from the DOM.
      */
     disconnectedCallback () {
+      this._canvas.removeEventListener('mousedown', this._onMousedown)
+      this._canvas.removeEventListener('mousemove', this._onMousemove)
+      this._canvas.removeEventListener('mouseup', this._onMouseup)
+    }
 
+    /**
+     * Draws a line(dot) on the canvas.
+     *
+     * @param {number} x - The mouse event's x-coordinate.
+     * @param {number} y - The mouse event's y-coordinate.
+     */
+    _draw (x, y) {
+      this._context.beginPath()
+      this._context.strokeStyle = this._color
+      this._context.lineWidth = this._lineWidth
+      this._context.lineCap = 'round'
+      this._context.moveTo(x, y)
+      this._context.lineTo(x, y)
+      this._context.stroke()
     }
   }
 )
